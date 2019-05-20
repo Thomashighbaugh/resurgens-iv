30eaadb97dc5aa23ae2a3ad65f902dcfd6f7a427
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2019-05-20 03:04:17 -0700

    add contact section

--- src/Components/Contact/Contact/Contact.js
+++ src/Components/Contact/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 //@material-ui imports
@@ -7,88 +6,104 @@ import { withStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import Snackbar from "@material-ui/core/Snackbar";
 import Typography from "@material-ui/core/Typography";
-import {Grid} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 
 const styles = () => ({
-    imgIcon: {
-        width: 200,
-        height: 200,
-        borderColor:"#d5d5d5",
-        borderWidth:"3px",
-        borderStyle:"double",
-        backgroundColor:"#000",
-        borderRadius:"25%"
-    },
-title:{
-        display:"flex",
-    alignItems:"center",
-},
-iconButton:{
-        backgroundColor: "#320575",
-    borderWidth:"3px",
-    borderStyle:"double",
-    borderColor:"d5d5d5",
-    borderRadius: "0%",
-}
+  imgIcon: {
+    width: 250,
+    height: 250,
+    borderColor: "#d5d5d5",
+    borderWidth: "3px",
+    borderStyle: "double",
+    backgroundColor: "#000",
+    borderRadius: "25%"
+  },
+  title: {
+    display: "flex",
+    alignItems: "center"
+  },
+  iconButton: {
+    backgroundColor: "#320575",
+    width: "90%",
+      borderRadius: "0%",
+
+  },
+  gridItem: {
+    display: "flex",
+    flexBasis: "20%",
+    borderColor: "#d5d5d5",
+    borderStyle: "double",
+    borderWidth: "3px",
+    margin: "1em",
+    flexDirection: "column",
+
+  }
 });
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isSnackbarOpen: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSnackbarOpen: false
+    };
+  }
 
-    toggleSnackbar() {
-        const { isSnackbarOpen } = this.state;
-        this.setState({ isSnackbarOpen: !isSnackbarOpen });
-    }
+  toggleSnackbar() {
+    const { isSnackbarOpen } = this.state;
+    this.setState({ isSnackbarOpen: !isSnackbarOpen });
+  }
 
-    render() {
-        const { src, link, classes, isLink,title } = this.props;
-        const { isSnackbarOpen } = this.state;
+  render() {
+    const { src, link, classes, isLink, title } = this.props;
+    const { isSnackbarOpen } = this.state;
 
-        return (
-            <button>
-                {isLink ? (
-                    <IconButton className={classes.iconButton} component="a" href={link} target="blank">
-                        <div>
-                        <img src={src} className={classes.imgIcon} alt={title} />
-                        <Typography component="h5" variant="h5">
-                            {title}
-                        </Typography>
-                        </div>
-                    </IconButton>
-                ) : (
-                    <CopyToClipboard text={link} onCopy={() => this.toggleSnackbar()}>
-                        <IconButton className={classes.iconButton}>
-                            <div>
-                            <img src={src} className={classes.imgIcon} alt={title} />
+    return (
+      <Grid className={classes.gridItem} item xs={12} style={{ height: "95%" }}>
+        {isLink ? (
+          <IconButton
+            className={classes.iconButton}
+            component="a"
+            href={link}
+            target="blank"
+          >
+            <div>
+              <img src={src} className={classes.imgIcon} alt={title} />
+              <Typography component="h5" variant="h5">
+                {title}
+              </Typography>
+            </div>
+          </IconButton>
+        ) : (
+          <CopyToClipboard text={link} onCopy={() => this.toggleSnackbar()}>
+            <IconButton className={classes.iconButton}
+                        component="a"
+                        target="blank">
+              <div>
+                <img src={src} className={classes.imgIcon} alt={title} />
 
-       <Typography component="h5" variant="h5">
-           {title}
-       </Typography>
-                            </div>
-       </IconButton>
-                    </CopyToClipboard>
-                )}
+                <Typography component="h5" variant="h5">
+                  {title}
+                </Typography>
+              </div>
+            </IconButton>
+          </CopyToClipboard>
+        )}
 
-                <Snackbar
-                    open={isSnackbarOpen}
-                    message={
-                        <Typography color="inherit">Email copied to clipboard!</Typography>
-                    }
-                    onClose={() => this.toggleSnackbar()}
-                    autoHideDuration={2000}
-                    anchorOrigin={{
-                        vertical: "bottom",
-                        horizontal: "right"
-                    }}
-                />
-                </button>
-        );
-    }
+        <Snackbar
+          open={isSnackbarOpen}
+          message={
+            <Typography color="inherit">Email copied to clipboard!</Typography>
+          }
+          onClose={() => this.toggleSnackbar()}
+          autoHideDuration={2000}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right"
+          }}
+        />
+      </Grid>
+    );
+  }
 }
 
 export default withStyles(styles)(Contact);
--- src/Components/Contact/Contacts.js
+++ src/Components/Contact/Contacts.js
@@ -3,28 +3,31 @@ import Contact from "./Contact/Contact";
 
 //@material-ui imports
 import { withStyles } from "@material-ui/core/styles";
-import {Divider, Grid, Typography} from "@material-ui/core";
+import { Divider, Grid, Typography } from "@material-ui/core";
 
 const styles = () => ({
-
   paper: {
     display: "flex",
-      flexDirection:"row",
-      maxWidth:"100%",
-      flexWrap:"wrap",
-      flexBasis:"33%",
-      margin:"2%",
-      // backgroundColor:"#320575"
-      marginBottom:"10%"
+    flexDirection: "row",
+    maxWidth: "100%",
+    flexWrap: "wrap",
+    flexBasis: "25%",
+      padding:"2em",
+    // backgroundColor:"#320575"
+    marginBottom: "10%",
+      margin:"5em"
   },
-header:{
-      marginBottom: "5%",
-    backgroundColor:"#320575",
-    padding:"1em"
-},
-    gridItem:{
-      margin: "2%"
-    }
+  header: {
+    marginBottom: "1em",
+    backgroundColor: "#320575",
+    padding: "1em",
+      marginRight:"2em"
+  },
+  contact: {
+    margin: "2%",
+      padding: "2%",
+
+  }
 });
 
 class Contacts extends Component {
@@ -32,69 +35,71 @@ class Contacts extends Component {
     const { classes } = this.props;
     return (
       <div className={classes.paper}>
-          <Typography component="h4" variant="h4" className={classes.header}>
-              Below are links to my profiles on various sites as well as my email and cell phone number. </Typography>
-          <Grid className={classes.gridItem}
-          item
-          xs={12}
-          style={{height: "95%"}}>
-        <Contact
-            className={classes.contact}
-          isLink={true}
-          src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iconsdb.com%2Ficons%2Fpreview%2Fwhite%2Fgithub-6-xxl.png&f=1"
-          link="https://github.com/Thomashighbaugh"
-          title="GitHub"
-        />
-
-        <Contact
+        <Typography component="h4" variant="h4" className={classes.header}>
+          Below are links to my profiles on various sites as well as my email
+          and cell phone number.{" "}
+        </Typography>
+          <Contact
             className={classes.contact}
             isLink={true}
-          src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png"
-          link="https://www.linkedin.com/in/thomas-leon-highbaugh"
-          title="LinkedIn"
-        />
+            src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.iconsdb.com%2Ficons%2Fpreview%2Fwhite%2Fgithub-6-xxl.png&f=1"
+            link="https://github.com/Thomashighbaugh"
+            title="GitHub"
+          />
+          <Contact
+              className={classes.contact}
+              isLink={true}
+              src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ffreevector.co%2Fwp-content%2Fuploads%2F2013%2F07%2Fgitlab.png&f=1"
+              link="https://gitlab.com/Thomashighbaugh"
+              title="GitLab"
+          />
 
-        <Contact
+          <Contact
+              className={classes.contact}
+              isLink={true}
+              src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fmaxcdn.icons8.com%2FShare%2Ficon%2FLogos%2Fbitbucket1600.png&f=1"
+              link="https://bitbucket.org/%7B79ec23ad-82c2-4f18-ae06-c0211ab78906%7D/"
+              title="BitBucket"
+          />
+
+          <Contact
             className={classes.contact}
+            isLink={true}
+            src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png"
+            link="https://www.linkedin.com/in/thomas-leon-highbaugh"
+            title="LinkedIn"
+          />
 
-          isLink={false}
-          src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ffiles.softicons.com%2Fdownload%2Fapplication-icons%2Fcircle-icons-by-martz90%2Fpng%2F256x256%2Fgmail.png&f=1"
-          link="thighbaugh@zoho.com"
-          title="Email"
-        />
-        <Contact
+          <Contact
             className={classes.contact}
+            isLink={false}
+            src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ffiles.softicons.com%2Fdownload%2Fapplication-icons%2Fcircle-icons-by-martz90%2Fpng%2F256x256%2Fgmail.png&f=1"
+            link="thighbaugh@zoho.com"
+            title="Email"
+          />
 
-          isLink={true}
-          src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ffreevector.co%2Fwp-content%2Fuploads%2F2013%2F07%2Fgitlab.png&f=1"
-          link="https://gitlab.com/Thomashighbaugh"
-        title="GitLab"
-        />
-        <Contact
+          <Contact
             className={classes.contact}
-
-          isLink={true}
-          src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fmaxcdn.icons8.com%2FShare%2Ficon%2FLogos%2Fbitbucket1600.png&f=1"
-          link="https://bitbucket.org/%7B79ec23ad-82c2-4f18-ae06-c0211ab78906%7D/"
-        title="BitBucket"
-        />
-        <Contact
+            isLink={true}
+            src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ficons.iconarchive.com%2Ficons%2Fuiconstock%2Fsocialmedia%2F512%2FDribbble-icon.png&f=1"
+            link="https://www.dribbble.com/thighbaugh"
+            title="Dribbble"
+          />
+          <Contact
             className={classes.contact}
+            isLink={true}
+            src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fblogs.chatham.edu%2Flibrary%2Ffiles%2F2017%2F11%2Finstagram_PNG10-1vdv6wi.png&f=1"
+            link="https://instagram.com/tlh-resurgens"
+            title="Instagram"
+          />
+          <Contact
+              className={classes.contact}
+              isLink={false}
+              src="https://cdn4.iconfinder.com/data/icons/smart-phones-technologies/512/android-phone.png"
+              link="+1 510-907-0654"
+              title="Cell"
+              />
 
-          isLink={true}
-          src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ficons.iconarchive.com%2Ficons%2Fuiconstock%2Fsocialmedia%2F512%2FDribbble-icon.png&f=1"
-          link="https://www.dribbble.com/thighbaugh"
-        title="Dribbble"
-        />
-        <Contact
-            className={classes.contact}
-
-          isLink={true}
-          src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fblogs.chatham.edu%2Flibrary%2Ffiles%2F2017%2F11%2Finstagram_PNG10-1vdv6wi.png&f=1"
-          link="https://instagram.com/tlh-resurgens"
-        title="Instagram"
-        />
-          </Grid>
       </div>
     );
   }
