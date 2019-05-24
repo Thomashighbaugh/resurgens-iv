9b61511ce1b388cc18cbd603d91957459f0dc33a
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2019-05-24 13:27:11 -0700

    modify background

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
-import BackgroundImagePage from "./Components/BackgroundImagePage/BackgroundImagePage";
 
 const theme = createMuiTheme({
   palette: {
